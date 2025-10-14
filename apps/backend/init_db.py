978392bb2ef5023823d08f58bddabc46290973d9
Hamid <hamidreza.darabi@hotmail.com>
2025-10-14 20:02:40 +0200

    Fix string formatting in database initialization script

--- apps/backend/init_db.py
+++ apps/backend/init_db.py
@@ -1,8 +1,9 @@
 """Initialize database tables from models"""
+
 from app_factory import create_app
 from database import db
 
-app = create_app('operations')
+app = create_app("operations")
 
 with app.app_context():
     print("Creating all tables...")
